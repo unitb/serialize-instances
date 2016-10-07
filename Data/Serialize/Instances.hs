b79b6cfc7c7828225d25969814977d25192a1aba
Simon Hudon <simon.hudon@gmail.com>
2016-10-07 18:45:43 -0400

    Initial definition of the instances

---
+++ Data/Serialize/Instances.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE StandaloneDeriving,DeriveGeneric #-}
+module Data.Serialize.Instances where
+
+import Data.List.NonEmpty
+import Data.Serialize
+import Data.Typeable
+import Data.Typeable.Internal
+
+import GHC.Generics
+
+class Serialize1 f where
+    put1 :: Serialize a => Putter (f a)
+    default put1 :: Serialize (f a) => Putter (f a)
+    put1 = put
+    get1 :: Serialize a => Get (f a)
+    default get1 :: Serialize (f a) => Get (f a)
+    get1 = get
+
+deriving instance Generic Fingerprint
+deriving instance Generic TypeRep
+
+instance Serialize Fingerprint where
+instance Serialize TyCon where
+    get = mkTyCon3 <$> get <*> get <*> get
+    put x = do
+        put $ tyConPackage x
+        put $ tyConModule x
+        put $ tyConName x
+instance Serialize TypeRep where
+
+instance Serialize1 NonEmpty where
+
+instance Serialize a => Serialize (NonEmpty a) where
---
+++ README.md
@@ -0,0 +1,5 @@
+# serialize-instances
+
+[![Build Status](https://travis-ci.org/literate-unitb/serialize-instances.svg?branch=master)](https://travis-ci.org/literate-unitb/serialize-instances)
+
+Instances for Serialize on NonEmpty and TypeRep
